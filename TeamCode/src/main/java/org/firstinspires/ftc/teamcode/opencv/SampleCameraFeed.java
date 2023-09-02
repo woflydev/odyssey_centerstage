fa1b3a75e8548819cc59c405c9929a96e43aa022
hashtable2020 <ilai1@eq.edu.au>
2023-09-02 21:34:57 +1000

    webcam stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/SampleCameraFeed.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode.opencv;
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp
+public class SampleCameraFeed extends LinearOpMode {
+
+    private boolean viewportPaused;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private OpenCvWebcam webcam = null;
+    private int cameraMonitorId = 0;
+    private int w = 640;
+    private int h = 480;
+
+    private boolean edgeMode = false;
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        cameraMonitorId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorId);
+        webcam.setPipeline(new SamplePipeline());
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(w,h, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Error", "Camera error");
+                telemetry.update();
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            if (gamepad1.y) {
+                edgeMode = !edgeMode;
+            }
+
+            if (gamepad1.a) {
+                webcam.stopStreaming();
+            }
+        }
+    }
+    class SamplePipeline extends OpenCvPipeline {
+        @Override
+        public void init(Mat input) {
+            telemetry.addLine("Pipeline processing first frame");
+        }
+        @Override
+        public Mat processFrame(Mat input) {
+            if (edgeMode) {
+                Mat tmp = new Mat();
+                Imgproc.Canny(input, tmp, 400, 800);
+                return tmp;
+            } else {
+                return input;
+            }
+        }
+
+        @Override
+        public void onViewportTapped() {
+            viewportPaused = !viewportPaused;
+
+            if (viewportPaused) {
+                webcam.pauseViewport();
+            } else {
+                webcam.resumeViewport();
+            }
+        }
+    }
+}
