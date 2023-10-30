bbfcd645d88df279e5b08f8b0749ac260d3b45fe
woflydev <wolfy.coding@gmail.com>
2023-10-30 14:52:26 +1000

    CHORE: add fsm annotation for clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/NewRobot_v8_FSM_FullRobot_v3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/NewRobot_v8_FSM_FullRobot_v3.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.drive.Robotv8.Robotv8_FSM_FullstackTesting;
 import org.firstinspires.ftc.teamcode.drive.Robotv8.Robotv8_FullstackTesting;
 
-@TeleOp()
+@TeleOp(name="TeleOp FSM v3", group="Final")
 public class NewRobot_v8_FSM_FullRobot_v3 extends Robotv8_FSM_FullstackTesting {
     public void MainLoop() {
         Mecanum();
