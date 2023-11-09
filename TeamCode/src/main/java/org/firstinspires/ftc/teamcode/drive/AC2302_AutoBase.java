80d20f2755193732f68ce92b4c630c44881a0b77
woflydev <wolfy.coding@gmail.com>
2023-11-09 12:34:50 +1000

    FEAT: add base auto - pending testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AC2302_AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AC2302_AutoBase.java
@@ -24,15 +24,117 @@ import org.openftc.easyopencv.OpenCvWebcam;
 public class AC2302_AutoBase extends FSM_Fullstack {
     private PropPipeline.Randomization randomization;
     private final ElapsedTime autoTimer = new ElapsedTime();
-    public RobotAlliance alliance = RobotAlliance.RED;
+    private RobotAlliance alliance;
+    private final int dir = alliance == RobotAlliance.RED ? 1 : -1;
     private Point r1;
     private Point r2;
     private Point r3;
-
     private final PropPipeline pipeline = new PropPipeline( alliance, r1, r2, r3 );
 
+    // note: custom behaviour -----------------------------------------------------------
+    public AC2302_AutoBase(RobotAlliance alliance, Point r1, Point r2, Point r3){
+        this.alliance = alliance;
+        this.r1 = r1;
+        this.r2 = r2;
+        this.r3 = r3;
+    }
+
+    public void MainInit() {
+        OpenCvWebcam webcam;
+
+        @SuppressLint("DiscouragedApi") int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId",
+                "id",
+                hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(pipeline);
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                FtcDashboard.getInstance().startCameraStream(webcam, 20);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // intentional noop
+            }
+        });
+
+        autoTimer.reset();
+        while (autoTimer.seconds() < 5) {
+            randomization = pipeline.getRandomization();
+            telemetry.addData("Randomization", randomization);
+            telemetry.update();
+        }
+        webcam.closeCameraDevice();
+    }
+
+    public void MainStart() {
+        randomization = pipeline.getRandomization();
+
+        VisualMove(0.8, 1, 1, false, false, 3);
+        AutoWait();
+
+        // note: drop off at correct spikemark
+        switch(randomization) {
+            case LOCATION_1: // note: left
+                break;
+            case LOCATION_2: // note： forward
+                VisualMove(0.5, 0.3, 0.3, false, false, 3);
+                AutoWait();
+                ExpelPixel();
+                AutoWait();
+                VisualMove(0.5, -0.3, -0.3, false, false, 3);
+                AutoWait();
+                VisualMove(0.6, -dir, dir, false, false, 3); // note: turn 90 deg on same tile.
+                break;
+            case LOCATION_3: // note: right
+                break;
+            default:
+                break;
+        }
+
+        VisualMove(0.6, -1.54, -1.54, false, false, 3);
+
+        // note: drop off at correct april tag
+        switch(randomization) {
+            case LOCATION_1: // note: left
+                break;
+            case LOCATION_2: // note： forward
+                VisualMove(0.5, 0.3, 0.3, false, false, 3);
+                AutoWait();
+                ExpelPixel();
+                AutoWait();
+                VisualMove(0.5, -0.3, -0.3, false, false, 3);
+                AutoWait();
+                VisualMove(0.6, -dir, dir, false, false, 3); // note: turn 90 deg on same tile.
+                break;
+            case LOCATION_3: // note: right
+                break;
+            default:
+                break;
+        }
+
+        GrabAndReady();
+        Delay(1000);
+        RaiseAndPrime(300);
+        Delay(1000);
+        DropAndReset();
+
+        VisualMove(0.7, 0.3, 0.3, false, false, 3);
+        VisualMove(0.7, 1, 1, true, false, 5); // note: strafe
+        VisualMove(0.5, dir, -dir, false, false, 3);
+        AutoWait();
+        VisualMove(0.5, 0.2, 0.2, true, true, 3);
+    }
+
+
+    // note: helper functions -----------------------------------------------------------
     private void ExpelPixel() {
-        intake.setPower(-0.3);
+        intake.setPower(-0.4);
         Delay(2000);
         intake.setPower(0);
     }
@@ -96,7 +198,7 @@ public class AC2302_AutoBase extends FSM_Fullstack {
         return ENCODER_TICKS_PER_TILE * input;
     }
 
-    private void EncoderMove(double power, double left, double right, boolean strafe, boolean strafeRight, double safetyTimeout) {
+    private void VisualMove(double power, double left, double right, boolean strafe, boolean strafeRight, double safetyTimeout) {
 
         int backLMTarget;
         int frontLMTarget;
@@ -164,79 +266,5 @@ public class AC2302_AutoBase extends FSM_Fullstack {
 
     private void AutoWait() {
         Delay(200);
-    }
-
-    public AC2302_AutoBase(RobotAlliance alliance, Point r1, Point r2, Point r3){
-        this.alliance = alliance;
-        this.r1 = r1;
-        this.r2 = r2;
-        this.r3 = r3;
-    }
-
-    public void MainInit() {
-        OpenCvWebcam webcam;
-
-        @SuppressLint("DiscouragedApi") int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId",
-                "id",
-                hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        webcam.setPipeline(pipeline);
-
-        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-                FtcDashboard.getInstance().startCameraStream(webcam, 20);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                // intentional noop
-            }
-        });
-
-        autoTimer.reset();
-        while (autoTimer.seconds() < 5) {
-            randomization = pipeline.getRandomization();
-            telemetry.addData("Randomization", randomization);
-            telemetry.update();
-        }
-        webcam.closeCameraDevice();
-    }
-
-    public void MainStart() {
-        int dir = alliance == RobotAlliance.RED ? 1 : -1;
-        randomization = pipeline.getRandomization();
-
-        switch(randomization) {
-            case LOCATION_1:
-                break;
-            case LOCATION_2:
-                break;
-            case LOCATION_3:
-                break;
-            default:
-                break;
-        }
-
-        /*GrabAndReady();
-        EncoderMove(0.8, 1, 1, false, false, 3);
-        AutoWait();
-        EncoderMove(0.6, -1 * dir, 1 * dir, false, false, 3);
-        AutoWait();
-        EncoderMove(0.6, -1.54, -1.54, false, false, 3);
-
-        RaiseAndPrime(400);
-        Delay(1500);
-        DropAndReset();
-
-        EncoderMove(0.7, 0.3, 0.3, false, false, 3);
-        // note: strafe
-        EncoderMove(0.7, 1, 1, true, false, 5);
-        EncoderMove(0.5, 1 * dir, -1 * dir, false, false, 3);
-        AutoWait();
-        EncoderMove(0.5, 0.2, 0.2, true, true, 3);*/
     }
 }
