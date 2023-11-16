ed60aedcd678377359edabfade341df847201cf1
woflydev <wolfy.coding@gmail.com>
2023-11-16 14:09:35 +1000

    FIX: updated backboard center values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robotv8/RR_DRIVE_ONLY_AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robotv8/RR_DRIVE_ONLY_AutoBase.java
@@ -44,15 +44,15 @@ public class RR_DRIVE_ONLY_AutoBase extends FSM_Fullstack {
     public Point r2;
     public Point r3;
 
-    public static final double INCHES_PER_TILE = 26;
+    public static final double INCHES_PER_TILE = 24;
 
     public static final Pose2d[] RED_STARTING_POSES = {
-            new Pose2d(11.5, -60, Math.toRadians(90)),
-            new Pose2d(-35, -60, Math.toRadians(90)),
+            new Pose2d(11, -63, Math.toRadians(90)),
+            new Pose2d(-35, -63, Math.toRadians(90)),
     };
     public static final Pose2d[] BLUE_STARTING_POSES = {
-            new Pose2d(11.5, 60, Math.toRadians(270)),
-            new Pose2d(-35, 60, Math.toRadians(270)),
+            new Pose2d(11.5, 63, Math.toRadians(270)),
+            new Pose2d(-35, 63, Math.toRadians(270)),
     };
     public static final Pose2d[] RED_PARKING_POSES = {
             // note: inner is first
@@ -64,19 +64,19 @@ public class RR_DRIVE_ONLY_AutoBase extends FSM_Fullstack {
             new Pose2d(50, 53.5, Math.toRadians(270))
     };
     public static final Pose2d[] BACKBOARD_CENTER_POSES = {
-            // note: red first
-            new Pose2d(53.5, -36.5, Math.toRadians(180)),
-            new Pose2d(53.5, 36.5, Math.toRadians(180)),
+            // note: red is first
+            new Pose2d(48.5, -36.5, Math.toRadians(180)),
+            new Pose2d(48.5, 36.5, Math.toRadians(180)),
     };
     public static final double[] YELLOW_PIXEL_VARIANCE = {
-            1.6,
-            1.4,
+            1,
             1.2,
+            1.4,
     };
     public static final double[] PURPLE_PIXEL_VARIANCE = {
-            1.5,
-            1,
-            0.5,
+            1.3,
+            0.8,
+            0.3,
     };
 
     // note: custom behaviour -----------------------------------------------------------
